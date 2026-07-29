f60239d90d70d61d62550bd9cdcdbd4041ec171c
balls674 <peepporp31@gmail.com>
2026-07-29 09:25:37 +0530

    Rename class from 'binsearch' to 'Task1'

--- binsearch.java
+++ binsearch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Arrays;
-public class binsearch {
+public class Task1 {
     public static void main(String args[]){
         Scanner inp= new Scanner(System.in);
         System.out.print("Enter array size: ");
